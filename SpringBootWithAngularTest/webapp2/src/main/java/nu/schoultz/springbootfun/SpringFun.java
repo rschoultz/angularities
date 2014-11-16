40687505ed3a3be35922dd5137ace69b439c5215
schoultz <rickard@schoultz.nu>
2014-11-17 00:22:51 +0100

    added actuate plugin so http://.../health etc works. Fails to limit the sensitive ones by application.properties. It seems to be fine but is not read.

--- SpringBootWithAngularTest/webapp2/src/main/java/nu/schoultz/springbootfun/SpringFun.java
+++ SpringBootWithAngularTest/webapp2/src/main/java/nu/schoultz/springbootfun/SpringFun.java
@@ -3,9 +3,14 @@ package nu.schoultz.springbootfun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@PropertySources(value = {@PropertySource("classpath:application.properties")})
 public class SpringFun {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringFun.class);
