67f3cab4efd6f8b4cdcca04eb7b46d3e16b83aa3
schoultz <rickard@schoultz.nu>
2014-11-16 12:23:06 +0100

    Made it resemble the example code closely. Even though it is gradle 1.11 and this is 2.2...

--- SpringBootWithAngularTest/webapp2/src/main/java/nu/schoultz/springbootfun/SpringFun.java
+++ SpringBootWithAngularTest/webapp2/src/main/java/nu/schoultz/springbootfun/SpringFun.java
@@ -7,12 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableAutoConfiguration
 public class SpringFun {
-
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringFun.class);
 		app.setShowBanner(false);
 		app.run(args);
-
 	}
-
 }
